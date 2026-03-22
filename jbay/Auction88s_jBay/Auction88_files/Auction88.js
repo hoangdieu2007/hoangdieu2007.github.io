7a02488dc931db59a470b2e799cdc6c872191f65
Brate Hwang <bratehwang@Brates-MBP.fpt>
2026-03-23 01:23:00 +0700

    update jbay site

---
+++ jbay/Auction88s_jBay/Auction88_files/Auction88.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20260323
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:-37.50})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Auction88_files/Auction88Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
